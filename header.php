6a28a24f1c472c1d4715b80e497b89ec3c895088
talha5131 <talhamalik4147@gmail.com>
2020-07-21 20:45:50 +0500

    new

--- header.php
+++ header.php
@@ -9,21 +9,32 @@
 	<header id="header" class="navbar-fixed-top header" role="banner">
 		<div class="container">
 			<div class="row">
-				<!-- Logo start -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<div class="navbar-brand navbar-bg">
-						<a href="index.php">
-							<img class="img-responsive" src="images/logo.png" alt="logo">
-						</a> 
-					</div>                   
-				</div><!--/ Logo end -->
+
 				<nav class="collapse navbar-collapse clearfix" role="navigation">
+					<!-- Logo start -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<div class="navbar-brand navbar-bg">
+							<a href="index.php">
+								<img class="img-responsive" src="images/logo.png" alt="logo">
+							</a> 
+						</div>                   
+					</div><!--/ Logo end -->
+					<ul class="nav navbar-nav navbar-left">
+						<li class="">
+							<a href="#" class="dropdown-toggle btn btn-primary cd-btn" style="padding-right: 29px; margin-top: 10px;
+							margin-left: -50px;" data-toggle="dropdown">CV Upload</a>
+						</li>
+						<li class="">
+							<a href="#" class="dropdown-toggle btn btn-primary cd-btn" style="padding-right: 29px; margin-top: 10px;
+							margin-left: 10px;" data-toggle="dropdown">Post A Job</a>
+						</li>
+					</ul>
 					<ul class="nav navbar-nav navbar-right">
 						<li class="active">
 							<a href="index.php" class="dropdown-toggle" data-toggle="dropdown">Home  </a>
@@ -34,72 +45,69 @@
 						<li><a href="career.php">Jobs</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle">Services <i class="fa fa-angle-down"></i> </a>
-							<div class="dropdown-menu">
-								
-							</div>
-								<div class="dropdown-menu">
-									<ul class="scrollable-menu" role="menu" style="height: auto; max-height: 550px; overflow-x: hidden;">
-										<li class="dropdown-item"><a href="service2.php">Catering</a></li>
-										<li class="dropdown-item"><a href="service4.php">Light Industrial Staff</a></li>
-										<li class="dropdown-item"><a href="service8.php">Construction</a></li>
-										<li class="dropdown-item"><a href="service9.php">Events & Promotions</a></li>
-										<li class="dropdown-item"><a href="service10.php">IT & Telecom</a></li>
-										<li class="dropdown-item"><a href="service11.php">Sales & Retail</a></li>
-										<li class="dropdown-item"><a href="service12.php">Hospitality</a></li>
-									</ul>
-								</div>
-							</li>
-							
-							
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <i class="fa fa-angle-down"></i></a>
-								<div class="dropdown-menu">
-									<ul>
-										
-										<li><a href="blog-item.php">How to achieve your bonus?</a></li>
-									</ul>
-								</div>
-							</li>
-							<li><a href="testimonial.php">Testimonials</a></li>
-							<li><a href="contact.php">Contact</a></li>
-							
-						</ul>
-						
-						
-					</nav><!--/ Navigation end -->
-					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-				</div><!--/ Row end -->
-			</div><!--/ Container end -->
-		</header><!--/ Header end -->
+							<div class="dropdown-menu mega-menu" style="background-color: rgba(81, 40, 79, 0.5); border: 1px solid rgba(81, 40, 79, 0.5); border-radius: 20px">
+								<div class="row">
+									<div class="col-sm-4">
+										<img src="images/catering/2705385.jpg" class="img-responsive">
+										<a href="service2.php"><h4>CATERING</h4></a>
+										<img src="images/light-industrial-staff/img3.jpg" class="img-responsive">
+										<a href="service4.php"><h4>Light Industrial Staff</h4></a>
 
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	</body>
-	</html>
+										<img src="images/construction/Construction-Laborers.jpg" class="img-responsive">
+										<a href="service8.php"><h4>Construction</h4></a>
+
+									</div>
+									<div class="col-sm-4">
+										<img src="images/events/events2.jpg" alt="Events" class="img-responsive">
+										<a href="service9.php"><h4>Events & Promotions</h4></a>
+
+										<img src="images/it-and-tech/tech1.jpg" class="img-responsive">
+										<a href="service10.php"><h4>IT & Telecom</h4></a>
+
+									</div>
+									<div class="col-sm-4">
+										<img src="images/hospitality/hospitality1.jpg" class="img-responsive">
+										<a href="service12.php"><h4>Hospitality</h4></a>
+										<img src="images/sales-and-retail/sales1.jpg" class="img-responsive">
+										<a href="service11.php"><h4>Sales & Retail</h4></a>
+									</div>
+								</div>
+<!-- 								<ul class="scrollable-menu" role="menu" style="height: auto; max-height: 550px; overflow-x: hidden;">
+									<li class="dropdown-item"><a href="service2.php">Catering</a></li>
+									<li class="dropdown-item"><a href="service4.php">Light Industrial Staff</a></li>
+									<li class="dropdown-item"><a href="service8.php">Construction</a></li>
+									<li class="dropdown-item"><a href="service9.php">Events & Promotions</a></li>
+									<li class="dropdown-item"><a href="service10.php">IT & Telecom</a></li>
+									<li class="dropdown-item"><a href="service11.php">Sales & Retail</a></li>
+									<li class="dropdown-item"><a href="service12.php">Hospitality</a></li>
+								</ul> -->
+							</div>
+						</li>
+
+
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <i class="fa fa-angle-down"></i></a>
+							<div class="dropdown-menu">
+								<ul>
+
+									<li><a href="blog-item.php">How to achieve your bonus?</a></li>
+								</ul>
+							</div>
+						</li>
+						<li><a href="testimonial.php">Testimonials</a></li>
+						<li><a href="contact.php">Contact</a></li>
+
+					</ul>
+
+
+				</nav><!--/ Navigation end -->
+			</div><!--/ Row end -->
+		</div><!--/ Container end -->
+	</header><!--/ Header end -->
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+</body>
+</html>
 
